ce0c2429a8822a78af2c605f54c98bb098626db0
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-18 07:08:05 +0000

    Add objectbox initial setup

--- lib/app/data/models/track_model.dart
+++ lib/app/data/models/track_model.dart
@@ -1,8 +1,19 @@
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
+import 'package:objectbox/objectbox.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:retip/app/domain/entities/track_entity.dart';
+
+@Entity()
+class Track {
+  @Id()
+  int id;
+
+  Track({
+    this.id = 0,
+  });
+}
 
 class TrackModel extends TrackEntity {
   TrackModel._({
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:retip/app/data/providers/just_audio_provider.dart';
-import 'package:retip/app/data/providers/on_audio_query_provider.dart';
-import 'package:retip/app/data/providers/shared_preferences_provider.dart';
-import 'package:retip/app/data/repositories/audio_repository_implementation.dart';
-import 'package:retip/app/data/repositories/library_repository_implementation.dart';
-import 'package:retip/app/data/repositories/theme_repository_implementation.dart';
-import 'package:retip/app/retip_app.dart';
-import 'package:retip/app/domain/repositories/audio_repository.dart';
-import 'package:retip/app/domain/repositories/library_repository.dart';
-import 'package:retip/app/data/providers/retip_audio.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'app/data/providers/just_audio_provider.dart';
+import 'app/data/providers/on_audio_query_provider.dart';
+import 'app/data/providers/retip_audio.dart';
+import 'app/data/providers/shared_preferences_provider.dart';
+import 'app/data/repositories/audio_repository_implementation.dart';
+import 'app/data/repositories/library_repository_implementation.dart';
+import 'app/data/repositories/theme_repository_implementation.dart';
+import 'app/domain/repositories/audio_repository.dart';
+import 'app/domain/repositories/library_repository.dart';
+import 'app/retip_app.dart';
+import 'objectbox.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +28,8 @@ void main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  await initObjectbox();
 
   const icon = 'drawable/ic_stat_name';
 
@@ -70,4 +79,34 @@ void main() async {
       provider: SharedPreferencesProvider(),
     ),
   ));
+}
+
+Future<void> initObjectbox() async {
+  // Objectbox setup
+  final appDir = await getApplicationDocumentsDirectory();
+  final dbPath = join(appDir.path, 'objectbox');
+
+  late Store store;
+  try {
+    store = await openStore(directory: dbPath);
+  } catch (e) {
+    final dbFile = File(join(dbPath, 'data.mdb'));
+    final lockFile = File(join(dbPath, 'lock.mdb'));
+
+    if (await dbFile.exists()) {
+      dbFile.delete();
+    }
+
+    if (await lockFile.exists()) {
+      lockFile.delete();
+    }
+
+    store = await openStore(directory: dbPath);
+  } finally {
+    GetIt.I.registerSingleton<Store>(store);
+
+    if (kReleaseMode == false && Admin.isAvailable()) {
+      GetIt.I.registerSingleton<Admin>(Admin(store));
+    }
+  }
 }
