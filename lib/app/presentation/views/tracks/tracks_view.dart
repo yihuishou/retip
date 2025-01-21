58593ec865fb84d0637687cf5e6654f468005571
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-21 21:47:17 +0000

    Display new track list with artwork from file

--- lib/app/presentation/views/tracks/tracks_view.dart
+++ lib/app/presentation/views/tracks/tracks_view.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:retip/app/domain/repositories/library_repository.dart';
+import 'package:retip/app/presentation/widgets/rp_icon_button.dart';
 
 // import 'bloc/tracks_bloc.dart';
+import '../../../../core/utils/sizer.dart';
+import '../../widgets/rp_list_tile.dart';
+import '../../widgets/rp_text.dart';
 import 'widgets/tracks_empty_widget.dart';
 
 class TracksView extends StatelessWidget {
@@ -23,9 +29,40 @@ class TracksView extends StatelessWidget {
             itemBuilder: (context, index) {
               final track = snapshot.requireData[index];
 
-              return ListTile(
-                title: Text(track.title),
-                subtitle: Text(track.artist ?? ''),
+              return RpListTile(
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(Sizer.x0_5),
+                  child: Image.file(
+                    fit: BoxFit.cover,
+                    width: Sizer.x5,
+                    height: Sizer.x5,
+                    cacheHeight: 4 * Sizer.x5.toInt(),
+                    cacheWidth: 4 * Sizer.x5.toInt(),
+                    File(track.artwork!),
+                    frameBuilder:
+                        (context, child, frame, wasSynchronouslyLoaded) {
+                      if (wasSynchronouslyLoaded) {
+                        return child;
+                      }
+
+                      return AnimatedOpacity(
+                        duration: const Duration(milliseconds: 250),
+                        opacity: frame == null ? 0 : 1,
+                        child: child,
+                      );
+                    },
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Icon(Icons.music_note);
+                    },
+                  ),
+                ),
+                title: RpText(track.title),
+                subtitle: RpText(track.artist ?? ''),
+                trailing: RpIconButton(
+                  icon: Icons.more_vert,
+                  onPressed: () {},
+                ),
+                onTap: () {},
               );
             },
           );
