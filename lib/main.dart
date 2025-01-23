b8cae03a25c13cdb4e19d0f42b3ac4afe9f1adc4
Piotr Rozpończyk <rozpo.dev@gmail.com>
2025-01-23 22:34:21 +0000

    Init objectbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:objectbox/objectbox.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:retip/app/data/providers/just_audio_provider.dart';
 import 'package:retip/app/data/providers/on_audio_query_provider.dart';
+import 'package:retip/app/data/providers/retip_audio.dart';
 import 'package:retip/app/data/providers/shared_preferences_provider.dart';
 import 'package:retip/app/data/repositories/audio_repository_implementation.dart';
 import 'package:retip/app/data/repositories/library_repository_implementation.dart';
 import 'package:retip/app/data/repositories/theme_repository_implementation.dart';
-import 'package:retip/app/retip_app.dart';
 import 'package:retip/app/domain/repositories/audio_repository.dart';
 import 'package:retip/app/domain/repositories/library_repository.dart';
-import 'package:retip/app/data/providers/retip_audio.dart';
+import 'package:retip/app/retip_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -22,11 +28,11 @@ void main() async {
     DeviceOrientation.portraitUp,
   ]);
 
-  const icon = 'drawable/ic_stat_name';
+  await initObjectbox();
 
   await JustAudioBackground.init(
     androidNotificationChannelId: 'dev.rozpo.retip.audio',
-    androidNotificationIcon: icon,
+    androidNotificationIcon: 'drawable/ic_stat_name',
     androidStopForegroundOnPause: true,
     androidNotificationOngoing: true,
   );
@@ -70,4 +76,34 @@ void main() async {
       provider: SharedPreferencesProvider(),
     ),
   ));
+}
+
+Future<void> initObjectbox() async {
+  // Objectbox setup
+  final appDir = await getApplicationDocumentsDirectory();
+  final dbPath = join(appDir.path, 'objectbox');
+
+  late Store store;
+  try {
+    store = await openStore(directory: dbPath);
+  } catch (e) {
+    final dbFile = File(join(dbPath, 'data.mdb'));
+    final lockFile = File(join(dbPath, 'lock.mdb'));
+
+    if (await dbFile.exists()) {
+      await dbFile.delete();
+    }
+
+    if (await lockFile.exists()) {
+      await lockFile.delete();
+    }
+
+    store = await openStore(directory: dbPath);
+  } finally {
+    GetIt.I.registerSingleton<Store>(store);
+
+    if (kReleaseMode == false && Admin.isAvailable()) {
+      GetIt.I.registerSingleton<Admin>(Admin(store));
+    }
+  }
 }
